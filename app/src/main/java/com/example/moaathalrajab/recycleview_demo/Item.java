b044a6a2db64738b57d5d50858839543bf327e8b
Moaath Alrajab <moaath@alrajab.net>
2018-10-04 00:04:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/moaathalrajab/recycleview_demo/Item.java
@@ -0,0 +1,17 @@
+package com.example.moaathalrajab.recycleview_demo;
+
+public class Item {
+
+    private String name;
+
+    public Item(String n) {
+        name = n;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
